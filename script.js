a6123bd72af019df2289cb07fafaf33caea62476
revo-lut <venum.yasha@gmail.com>
2025-02-02 10:45:50 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Приклад JavaScript для активного пункту меню
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function () {
+    document.querySelectorAll('.nav-links a').forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
